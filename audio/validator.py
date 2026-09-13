67b426528773e15a0bd11679137bb97d8206553a
Saikrishnakmnr <101177829+Saikrishnakmnr@users.noreply.github.com>
2026-09-13 22:53:28 +0530

    Delete audio directory

--- audio/validator.py
+++
@@ -1,4 +0,0 @@
-def validate_audio(audio_bytes: bytes) -> bool:
-    if not audio_bytes or len(audio_bytes) < 44:
-        return False
-    return audio_bytes[:4] == b'RIFF' and audio_bytes[8:12] == b'WAVE'

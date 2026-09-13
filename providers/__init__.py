9a805f523454790057384868baa23bc7b7b05598
Saikrishnakmnr <101177829+Saikrishnakmnr@users.noreply.github.com>
2026-09-13 23:30:53 +0530

    Update __init__.py

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,8 +1,6 @@
 from .base import BaseMusicProvider, GenerationResult
-from .local import LocalFallbackProvider
 
 __all__ = [
     "BaseMusicProvider",
     "GenerationResult",
-    "LocalFallbackProvider",
 ]
